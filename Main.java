bf23e812cdf3905335cb4aca596fe247ba889d38
senthilv-2004 <160302340+senthilv-2004@users.noreply.github.com>
2025-02-12 11:41:40 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,29 @@
-class Main
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
 {
-    public static void main (String args[])
-    {
-        System.out.println("hello world");
-    }
+	public static void main(String[] args) {
+	    byte a=93;
+	    byte b=45;
+	    byte c=(byte) (a+b);
+		System.out.println("a+b:" + c);
+		 byte d=(byte) (a-b);
+		 	System.out.println("a-b:" + d);
+		 	 byte e=(byte) (a*b);
+		 	 	System.out.println("a*b:" + e);
+		 	 		 byte f=(byte) (a/b);
+		 	 		 	System.out.println("a/b:" + f);
+		 	 		 		 byte g=(byte) (a%b);
+		 	 		 		 	 	System.out.println("a%b:" + g);
+		 	 		 	
+		 	 		 
+		 	 	
+		
+	}
 }
