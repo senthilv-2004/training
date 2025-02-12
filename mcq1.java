49ebc38d1fb9823e71373c1d05c6325cdda78f75
senthilv-2004 <160302340+senthilv-2004@users.noreply.github.com>
2025-02-12 16:09:28 +0530

    Add files via upload

---
+++ mcq1.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class mcq1
+{
+	public static void main(String[] args) {
+	    int n=7;
+	  for(;;)
+	  {
+	      if(n>3)
+	      System.out.println(n);
+	      else
+	      break;
+	      n--;
+	      
+	  }
+	}}
---
+++ mcq2.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class mcq2
+{
+	public static void main(String[] args) {
+	    int i=1;
+	    while(i>0)
+	    {
+	    System.out.println(i--);
+	    i--;
+	    }}}
+	 
