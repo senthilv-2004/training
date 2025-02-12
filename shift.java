5f9bb9f7d7e1acfc96586179230a5fa54d9fc52e
senthilv-2004 <160302340+senthilv-2004@users.noreply.github.com>
2025-02-12 13:01:40 +0530

    Add files via upload

---
+++ shift.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Bitwise
+{
+	public static void main(String[] args) {
+	 int a=10;
+	 System.out.println("right shift:"+(a<<1));
+	 	 System.out.println("left shift:"+(a>>1));
+	}
+}
