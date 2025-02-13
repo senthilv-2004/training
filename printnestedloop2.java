9805e14ab931f4949fe49657094f2f65a5593ae5
senthilv-2004 <160302340+senthilv-2004@users.noreply.github.com>
2025-02-13 12:00:02 +0530

    Add files via upload

---
+++ printnestedloop2.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class printnestedloop2
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = in.nextInt();
+       int i,j;
+       for(i=0;i<=n;i++)
+       {
+           for(j=0;j<=i;j++)
+           {
+               System.out.print(i+1);
+           }
+           System.out.println();
+       }}}
